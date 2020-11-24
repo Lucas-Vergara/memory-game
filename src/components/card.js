7a012ce60f2a271cfe605cb6568bf41d1b46a682
Lucas-Vergara <lucasvergara1@gmail.com>
2020-11-24 16:53:53 -0300

    first commit

---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const Card = (image) => {
+    const [img, setImg] = useState (image)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Card
